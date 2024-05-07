15b154283f0fb1da1b034b4777bdd7f8332d5d98
caldwell-matthew <caldwell.j.matthew@gmail.com>
2024-05-07 16:47:12 -0700

    wip

--- client/index.js
+++ client/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient, { createNetworkInterface } from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
-import { Router, hashHistory, Route, IndexRoute } from 'react-router';
+import { Router, hashHistory, Route } from 'react-router';
 
 import App from './components/app';
+import LoginForm from './components/LoginForm';
 
 const networkInterface = createNetworkInterface({
   uri: '/graphql',
@@ -23,6 +24,7 @@ const Root = () => {
     <ApolloProvider client={client} >
       <Router history={hashHistory}>
         <Route path="/" component={App}>
+          <Route path="login" component={LoginForm}></Route>
         </Route>
       </Router>
     </ApolloProvider>
