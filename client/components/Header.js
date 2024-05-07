055a4724aeeded33fd01da408ce7b04add7e661a
caldwell-matthew <caldwell.j.matthew@gmail.com>
2024-05-07 15:41:21 -0700

    fixz

--- client/components/Header.js
+++ client/components/Header.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { graphql } from 'react-apollo'; 
 import query from '../queries/CurrentUser';
 import { Link } from 'react-router';
-import mutation from '../mutations';
+import mutation from '../mutations/logout';
 
 class Header extends Component {
   onLogoutClick() {
